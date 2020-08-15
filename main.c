54af4f0ff77c1418570c1057ff5ebe18047c8134
neo <xiongyu0523@gmail.com>
2020-08-15 22:25:29 +0800

    first commit

---
+++ main.c
@@ -0,0 +1,12 @@
+﻿// This minimal Azure Sphere app prints "High Level Application" to the debug
+// console and exits with status 0.
+
+#include <applibs/log.h>
+
+int main(void)
+{
+    // Please see the extensible samples at: https://github.com/Azure/azure-sphere-samples
+    // for examples of Azure Sphere platform features
+    Log_Debug("High Level Application\n");
+    return 0;
+}
